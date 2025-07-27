a67d3c65313c41c4a57f41b18147ca72689e8205
Paweł Boroń <pawcio2001.boron@interia.pl>
2025-07-27 22:25:33 +0200

    Dodanie endpointu Delete dla device

--- ServiceLog/Controllers/DeviceController.cs
+++ ServiceLog/Controllers/DeviceController.cs
@@ -39,5 +39,28 @@ namespace ServiceLog.Controllers
             }
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDeviceAsync([FromRoute] string id)
+        {
+            try
+            {
+                var result = await _deviceService.DeleteDeviceAsync(id);
+                if (result.Success)
+                {
+                    return Ok(result);
+                }
+                return result.ErrorCode switch
+                {
+                    DeviceErrorCode.DeviceNotFound => NotFound(result),
+                    DeviceErrorCode.InvalidData => Unauthorized(result),
+                    DeviceErrorCode.EmptyFields => BadRequest(result),
+                    _ => BadRequest(result)
+                };
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Error:: {e.Message}");
+            }
+        }
     }
 }
